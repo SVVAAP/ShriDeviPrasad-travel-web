dcf4d8d0cade5b4e458a49b566bb43864f48636b
SVVAAP <workwithsvvaao@gmail.com>
2025-01-12 18:56:23 +0530

    hii

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './App.css';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './App.css'
+import './index.css'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ShriDeviPrasad-travel-web/',
+  
   plugins: [react()],
   server: {
-    port: 3000,  // Define the port for the development server
-    open: true  // Automatically open the browser when the server starts
+    port: 3000,  
+    open: true  
   }
 })
+
+
